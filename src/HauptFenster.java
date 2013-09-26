2bb4b431b2b56a2d2dfbace1886895991f650cfe
Marvin Bellmann <marvin.bellmann@gmail.com>
2013-09-26 12:20:00 +0200

    GUI updated

--- src/HauptFenster.java
+++ src/HauptFenster.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -15,132 +16,136 @@ import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableModel;
 
-
 public class HauptFenster {
 
-    public static int messagesLeft = 10;
-    static JFrame frame;
-    private JTable table;
-    private JTextField txtSuche;
-    static String username="AdminTest";
-    //static int messagesLeft=0;
+	private static int maxWidth = GraphicsEnvironment
+			.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+	private static int maxHeight = GraphicsEnvironment
+			.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+	public static int messagesLeft = 10;
+	static JFrame frame;
+	private static int width = 250;
+	private static int height = 600;
+	private static int border = 5;
+	private JTable table;
+	private JTextField txtSuche;
+	static String username = "AdminTest";
 
-    /**
-     * Launch the application.
-     */
-    public static void main(String[] args) {
-	EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		try {
-		    HauptFenster window = new HauptFenster();
-		    window.frame.setVisible(true);
-		} catch (Exception e) {
-		    e.printStackTrace();
-		}
-	    }
-	});
-	
-	
-	
-	
-	
-	//Server server = new Server();
-	Client client = new Client("localhost");
-	
-	
-	
-    }
+	// static int messagesLeft=0;
 
-    /**
-     * Create the application.
-     */
-    public HauptFenster() {
-	initialize();
-    }
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HauptFenster window = new HauptFenster();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
-    /**
-     * Initialize the contents of the frame.
-     */
-    private void initialize() {
-	frame = new JFrame();
-	frame.setResizable(false);
-	frame.setBounds(1100, 100, 191, 400);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.getContentPane().setLayout(null);
-	
-	JLabel label = new JLabel("Online");
-	label.setHorizontalAlignment(SwingConstants.CENTER);
-	label.setForeground(Color.GREEN);
-	label.setFont(new Font("Tahoma", Font.BOLD, 14));
-	label.setBounds(89, 34, 83, 14);
-	frame.getContentPane().add(label);
-	
-	JScrollPane scrollPane = new JScrollPane();
-	scrollPane.setBounds(14, 136, 158, 226);
-	frame.getContentPane().add(scrollPane);
-	
-	table = new JTable();
-	table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-	table.addMouseListener(new MouseAdapter() {
-		@Override
-		public void mousePressed(MouseEvent arg0) {
-		    
-		    
-		    ChatFenster chatFenster = new ChatFenster();
-		    chatFenster.domain((String)table.getValueAt(table.getSelectedRow(),2));
-		  System.out.println("Chatfenster mit "+table.getValueAt(table.getSelectedRow(),2) + " geöffnet.");
-		    
-		}
-	});
-	//table.setEditingColumn(false);
-	//table.setE
-	//table.
-	table.setModel(new DefaultTableModel(
-		new Object[][] {
-			{"Bild1", "Online", "Marvin"},
-			{"Bild2", "Online", "Franz"},
-			{"Bild3", "Offline", "Lappen"},
-		},
-		new String[] {
-			"Profilbild", "Status", "Name"
-		}
-	));
-	scrollPane.setViewportView(table);
-	
-	JLabel lblKontakte = new JLabel("Kontakte");
-	lblKontakte.setFont(new Font("Tahoma", Font.BOLD, 14));
-	lblKontakte.setHorizontalAlignment(SwingConstants.CENTER);
-	lblKontakte.setBounds(14, 97, 158, 14);
-	frame.getContentPane().add(lblKontakte);
-	
-	JLabel label_2 = new JLabel("");
-	label_2.setIcon(new ImageIcon(HauptFenster.class.getResource("/data/1.jpg")));
-	label_2.setBounds(14, 14, 69, 69);
-	frame.getContentPane().add(label_2);
-	
-	JLabel lblSvenole = new JLabel("Sven-Ole");
-	lblSvenole.setHorizontalAlignment(SwingConstants.CENTER);
-	lblSvenole.setFont(new Font("Tahoma", Font.BOLD, 14));
-	lblSvenole.setBounds(89, 15, 84, 14);
-	frame.getContentPane().add(lblSvenole);
-	
-	txtSuche = new JTextField();
-	txtSuche.setForeground(Color.LIGHT_GRAY);
-	txtSuche.setText("Suche");
-	txtSuche.setBounds(14, 113, 158, 20);
-	frame.getContentPane().add(txtSuche);
-	txtSuche.setColumns(10);
-	
-	JPanel panel = new JPanel();
-	panel.setBackground(Color.LIGHT_GRAY);
-	panel.setForeground(Color.ORANGE);
-	panel.setBounds(7, 94, 171, 271);
-	frame.getContentPane().add(panel);
-	
-	JPanel panel_2 = new JPanel();
-	panel_2.setForeground(Color.ORANGE);
-	panel_2.setBackground(Color.LIGHT_GRAY);
-	panel_2.setBounds(7, 7, 171, 82);
-	frame.getContentPane().add(panel_2);
-    }
+		// Server server = new Server();
+		Client client = new Client("localhost");
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public HauptFenster() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(maxWidth - width - 50, (maxHeight - height) / 2, width,
+				height);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(border, 150, width - 15, height - 185);
+		frame.getContentPane().add(scrollPane);
+
+		table = new JTable();
+		table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+
+				ChatFenster chatFenster = new ChatFenster();
+				chatFenster.domain((String) table.getValueAt(
+						table.getSelectedRow(), 2));
+				System.out.println("Chatfenster mit "
+						+ table.getValueAt(table.getSelectedRow(), 2)
+						+ " geöffnet.");
+			}
+		});
+		// table.setEditingColumn(false);
+		// table.setE
+		// table.
+		table.setModel(new DefaultTableModel(new Object[][] {
+				{ "Bild1", "Online", "Marvin" },
+				{ "Bild2", "Online", "Franz" },
+				{ "Bild3", "Offline", "Lappen" }, }, new String[] {
+				"Profilbild", "Status", "Name" }));
+		scrollPane.setViewportView(table);
+
+		// ProfilPanel
+		JPanel panelProfil = new JPanel();
+		panelProfil.setForeground(Color.ORANGE);
+		panelProfil.setBackground(Color.LIGHT_GRAY);
+		panelProfil.setBounds(border, border, width - 15, 80);
+		panelProfil.setLayout(null);
+
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(HauptFenster.class
+				.getResource("/data/1.jpg")));
+		label_2.setBounds(border, border, 69, 69);
+		panelProfil.add(label_2);
+
+		JLabel lblSvenole = new JLabel("Sven-Ole");
+		lblSvenole.setHorizontalAlignment(SwingConstants.LEFT);
+		lblSvenole.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblSvenole.setBounds((border * 2) + (69 + border), border, 90, 14);
+		panelProfil.add(lblSvenole);
+
+		JLabel label = new JLabel("Online");
+		label.setHorizontalAlignment(SwingConstants.LEFT);
+		label.setForeground(Color.GREEN);
+		label.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label.setBounds((border * 2) + (69 + border), (border * 2) + 14, 90, 14);
+		panelProfil.add(label);
+
+		frame.getContentPane().add(panelProfil);
+
+		// KontaktPanel
+		JPanel panelKontakt = new JPanel();
+		panelKontakt.setBackground(Color.LIGHT_GRAY);
+		panelKontakt.setForeground(Color.ORANGE);
+		panelKontakt.setBounds(border, 80 + (border * 2), width - 15, 50);
+		panelKontakt.setLayout(null);
+
+		JLabel lblKontakte = new JLabel("Kontakte");
+		lblKontakte.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblKontakte.setHorizontalAlignment(SwingConstants.CENTER);
+		lblKontakte.setBounds(border, border, width - (border * 5), 14);
+		panelKontakt.add(lblKontakte);
+
+		txtSuche = new JTextField();
+		txtSuche.setForeground(Color.LIGHT_GRAY);
+		txtSuche.setText("Suche");
+		txtSuche.setHorizontalAlignment(SwingConstants.CENTER);
+		txtSuche.setBounds(border, (border * 2) + 14, width - (border * 5), 20);
+		panelKontakt.add(txtSuche);
+
+		frame.getContentPane().add(panelKontakt);
+	}
 }
