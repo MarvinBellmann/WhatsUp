00452efdf04a3221a801c398771073f2a14a05a4
Marvin Bellmann <marvin.bellmann@gmail.com>
2013-10-24 23:53:06 +0200

    Layout für Hauptfenster umgesetzt

--- src/Client/HauptFenster.java
+++ src/Client/HauptFenster.java
@@ -16,7 +16,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
@@ -27,6 +26,7 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 
+import net.miginfocom.swing.MigLayout;
 import SendData.SQLData;
 
 public class HauptFenster {
@@ -36,11 +36,11 @@ public class HauptFenster {
 			EtchedBorder.RAISED, Color.darkGray, Color.lightGray);
 
 	static JFrame frame;
+	static GradientPanel mainPanel;
 	static String desktopPath;
 	public static int messagesLeft = 10;
 	private static int width = 250;
-	private static int height = 587;
-	private static int border = 5;
+	private static int height = 600;
 	static JTable table;
 	static ImageIcon i1;
 	static ImageIcon i2;
@@ -53,8 +53,8 @@ public class HauptFenster {
 	static ImageIcon i9;
 	static ImageIcon iDB;
 	public static JLabel label_2;
-	JLabel lblUsername ;
-	
+	JLabel lblUsername;
+
 	private JTextField txtSuche;
 	public static JLabel statuslabel;
 	int startX, startY;
@@ -64,6 +64,9 @@ public class HauptFenster {
 
 	/**
 	 * Create the application.
+	 */
+	/**
+	 * @wbp.parser.constructor
 	 */
 	public HauptFenster(String user, String pw, String server, int x, int y) {
 		username = user;
@@ -142,52 +145,39 @@ public class HauptFenster {
 			}
 
 			for (int i = 0; i < gesplittet.length; i = i + 3) {
-			    
-			   // case 
-			    ImageIcon avatarImage = new ImageIcon();
-				    switch(gesplittet[i + 2]){ 
-				        case "1": 
-				          //  System.out.println("pic1"); 
-				            avatarImage =i1;
-				            break; 
-				        case "2": 
-				          //  System.out.println("pic2"); 
-				            avatarImage =i2;
-				            break; 
-				        case "3": 
-				           // System.out.println("pic3"); 
-				            avatarImage =i3;
-				            break; 
-				        case "4": 
-				          //  System.out.println("pic4"); 
-				            avatarImage =i4;
-				            break; 
-				        case "5": 
-				           // System.out.println("pic5"); 
-				            avatarImage =i5;
-				            break; 
-				        case "6": 
-				          //  System.out.println("pic6"); 
-				            avatarImage =i6;
-				            break; 
-				        case "7": 
-				          //  System.out.println("pic7"); 
-				            avatarImage =i7;
-				            break; 
-				        case "8": 
-				          //  System.out.println("pic8"); 
-				            avatarImage =i8;
-				            break; 
-				        case "9": 
-				          //  System.out.println("pic9"); 
-				            avatarImage =i9;
-				            break; 
-				        default: 
-				          //  System.out.println("pic1"); 
-				            avatarImage =i1;
-				        } 
-				    
-				    
+				ImageIcon avatarImage = new ImageIcon();
+				switch (gesplittet[i + 2]) {
+				case "1":
+					avatarImage = i1;
+					break;
+				case "2":
+					avatarImage = i2;
+					break;
+				case "3":
+					avatarImage = i3;
+					break;
+				case "4":
+					avatarImage = i4;
+					break;
+				case "5":
+					avatarImage = i5;
+					break;
+				case "6":
+					avatarImage = i6;
+					break;
+				case "7":
+					avatarImage = i7;
+					break;
+				case "8":
+					avatarImage = i8;
+					break;
+				case "9":
+					avatarImage = i9;
+					break;
+				default:
+					avatarImage = i1;
+				}
+
 				model.addRow(new Object[] { avatarImage, gesplittet[i + 1],
 						gesplittet[i] });
 			}
@@ -215,7 +205,7 @@ public class HauptFenster {
 				table.setRowHeight(row, rowHeight);
 				table.getColumnModel().getColumn(0).setPreferredWidth(rowWidth);
 			}
-			
+
 			table.repaint();
 
 		} catch (Exception e) {
@@ -269,10 +259,8 @@ public class HauptFenster {
 		i8 = new ImageIcon(getClass().getResource("/data/8.png"));
 		i9 = new ImageIcon(getClass().getResource("/data/9.jpg"));
 		iDB = new ImageIcon(getClass().getResource("/data/database.png"));
-		//i91 = new ImageIcon(getClass().getResource("/data/2.png"));
-		
-		
-		
+		// i91 = new ImageIcon(getClass().getResource("/data/2.png"));
+
 		desktopPath = System.getProperty("user.home") + "/Desktop";
 		desktopPath = desktopPath.replace("\\", "/");
 		System.out.println("*** Anmeldungsversuch als: " + username);
@@ -293,8 +281,78 @@ public class HauptFenster {
 		frame.setResizable(false);
 		frame.setBounds(startX, startY, width, height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 
+		mainPanel = new GradientPanel(new Color(27, 130, 165), new Color(204,
+				204, 255), width, height);
+		mainPanel
+				.setLayout(new MigLayout("fill", "[80px:n:80px][]",
+						"[30px:n:30px][45px::45px,top][40px::40px,bottom][30px::30px][]"));
+
+		label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(HauptFenster.class
+				.getResource("/data/1.jpg")));
+		mainPanel.add(label_2, "span 1 2");
+
+		lblUsername = new JLabel(username);
+		lblUsername.setHorizontalAlignment(SwingConstants.LEFT);
+		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblUsername.setForeground(Color.white);
+		mainPanel.add(lblUsername, "wrap");
+
+		statuslabel = new JLabel("Offline");
+		statuslabel.setHorizontalAlignment(SwingConstants.LEFT);
+		statuslabel.setForeground(Color.RED);
+		statuslabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		mainPanel.add(statuslabel, "wrap");
+
+		JLabel lblKontakte = new JLabel("Kontakte");
+		lblKontakte.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblKontakte.setForeground(Color.white);
+		lblKontakte.setHorizontalAlignment(SwingConstants.CENTER);
+		mainPanel.add(lblKontakte, "spanx 2, growx, wrap");
+
+		txtSuche = new JTextField();
+		txtSuche.setText("");
+		txtSuche.setBorder(raisedetched);
+		txtSuche.setHorizontalAlignment(SwingConstants.CENTER);
+		mainPanel.add(txtSuche, "growx, spanx 2, split2");
+
+		JButton btnKontaktSuche = new JButton("+");
+		btnKontaktSuche.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+
+				if (txtSuche.getText().equalsIgnoreCase(lblUsername.getText()) == false) {
+					Client.sendSQL(new SQLData(
+							"UPDATE user set status='Online' where username like '"
+									+ username + "'", 'i'));
+
+					Client.sendSQL(new SQLData(
+							"INSERT IGNORE INTO contacts (username,contact) values ('"
+									+ username + "','" + txtSuche.getText()
+									+ "') ", 'i', username));
+					Client.sendSQL(new SQLData(
+							"INSERT IGNORE INTO contacts (username,contact) values ('"
+									+ txtSuche.getText() + "','" + username
+									+ "')", 'i', username));
+
+					txtSuche.setText("");
+				}
+			}
+
+		});
+		btnKontaktSuche.setFont(new Font("Tahoma", Font.BOLD, 12));
+		mainPanel.add(btnKontaktSuche, "wrap, w 50px::50px");
+
+		if (username.equalsIgnoreCase("Admin")) {
+			btnKontaktSuche.setEnabled(false);
+			txtSuche.setText("Admin hat alle");
+			txtSuche.setEnabled(false);
+			txtSuche.setEditable(false);
+		}
+
+		// Tabelle
 		DefaultTableModel model = new DefaultTableModel(new Object[][] {,},
 				new String[] { "Profilbild", "Status", "Name" });
 
@@ -307,21 +365,28 @@ public class HauptFenster {
 			public boolean isCellEditable(int x, int y) {
 				return false;
 			}
-			
-			public Component prepareRenderer(TableCellRenderer renderer, int row, int column)
-			{
+
+			public Component prepareRenderer(TableCellRenderer renderer,
+					int row, int column) {
 				Component c = super.prepareRenderer(renderer, row, column);
 
-				//  Color row based on a cell value
+				// Color row based on a cell value
 
-				if (!isRowSelected(row))
-				{
-					//c.setBackground(getBackground());
+				if (!isRowSelected(row)) {
+					// c.setBackground(getBackground());
 					int modelRow = convertRowIndexToModel(row);
-					String type = (String)getModel().getValueAt(modelRow, 1);
-					if ("Online".equals(type)){ c.setBackground(new Color(190, 250, 190)); c.setForeground(Color.GREEN.darker()); c.setFont(new Font("Miriam", Font.BOLD, 14));}
-					if ("Offline".equals(type)) {c.setBackground(new Color(250, 190, 190));c.setForeground(Color.RED.darker()); c.setFont(new Font("Miriam", Font.PLAIN, 14));}
-					//table.repaint();
+					String type = (String) getModel().getValueAt(modelRow, 1);
+					if ("Online".equals(type)) {
+						c.setBackground(new Color(190, 250, 190));
+						c.setForeground(Color.GREEN.darker());
+						c.setFont(new Font("Miriam", Font.BOLD, 14));
+					}
+					if ("Offline".equals(type)) {
+						c.setBackground(new Color(250, 190, 190));
+						c.setForeground(Color.RED.darker());
+						c.setFont(new Font("Miriam", Font.PLAIN, 14));
+					}
+					// table.repaint();
 				}
 
 				return c;
@@ -331,7 +396,6 @@ public class HauptFenster {
 		table.setPreferredScrollableViewportSize(table.getPreferredSize());
 		table.getSelectionModel().setSelectionMode(
 				ListSelectionModel.SINGLE_SELECTION);
-		table.setBounds(border, 130 + (border * 4), width - 15, 400);
 		table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
 		table.addMouseListener(new MouseAdapter() {
 			@Override
@@ -359,170 +423,60 @@ public class HauptFenster {
 		});
 
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(border, 130 + (border * 4), width - 15, 400);
 		scrollPane.setBorder(raisedetched);
 		scrollPane.setBackground(new Color(190, 190, 255));
+		mainPanel.add(scrollPane, "spanx 2,grow");
 
-		frame.getContentPane().add(scrollPane);
-
-		// ProfilPanel
-		JPanel panelProfil = new JPanel();
-		panelProfil.setBounds(border, border, width - 15, 80);
-		panelProfil.setLayout(null);
-		panelProfil.setOpaque(false);
-
-		label_2 = new JLabel("");
-		label_2.setIcon(new ImageIcon(HauptFenster.class
-				.getResource("/data/1.jpg")));
-		label_2.setBounds(border, border, 69, 69);
-		panelProfil.add(label_2);
-
-		lblUsername = new JLabel(username);
-		lblUsername.setHorizontalAlignment(SwingConstants.LEFT);
-		lblUsername.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblUsername.setForeground(Color.white);
-		lblUsername.setBounds((border * 2) + (69 + border), border, 90, 14);
-
-		panelProfil.add(lblUsername);
-
-		statuslabel = new JLabel("Offline");
-		statuslabel.setHorizontalAlignment(SwingConstants.LEFT);
-		statuslabel.setForeground(Color.RED);
-		statuslabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-		statuslabel.setBounds((border * 2) + (69 + border), (border * 2) + 14,
-				90, 14);
-		panelProfil.add(statuslabel);
-
-		frame.getContentPane().add(panelProfil);
-
-		// KontaktPanel
-		JPanel panelKontakt = new JPanel();
-		panelKontakt.setBounds(border, 80 + (border * 2), width - 15, 50);
-		panelKontakt.setLayout(null);
-		panelKontakt.setOpaque(false);
-
-		JLabel lblKontakte = new JLabel("Kontakte");
-		lblKontakte.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblKontakte.setForeground(Color.white);
-		lblKontakte.setHorizontalAlignment(SwingConstants.CENTER);
-		lblKontakte.setBounds(border, border, width - (border * 5), 14);
-
-		panelKontakt.add(lblKontakte);
-
-		txtSuche = new JTextField();
-		//txtSuche.setForeground(Color.LIGHT_GRAY);
-		txtSuche.setText("");
-		txtSuche.setBorder(raisedetched);
-		txtSuche.setHorizontalAlignment(SwingConstants.CENTER);
-		txtSuche.setBounds(border, (border * 2) + 14, width - (border * 30), 20);
-		panelKontakt.add(txtSuche);
-		
-		
-		JButton btnKontaktSuche = new JButton("Hinzufügen");
-		btnKontaktSuche.setBounds(border*21, (border * 2) + 14, 120, 20);
-		btnKontaktSuche.addActionListener(new ActionListener() {
-
-		    @Override
-		    public void actionPerformed(ActionEvent arg0) {
-			
-			
-			if(txtSuche.getText().equalsIgnoreCase(lblUsername.getText())==false){
-			 Client.sendSQL(new SQLData("UPDATE user set status='Online' where username like '"+username+"'",'i'));
-			    
-			   Client.sendSQL(new SQLData("INSERT IGNORE INTO contacts (username,contact) values ('"+ username+ "','"+txtSuche.getText()+"') ",'i',username));
-			   Client.sendSQL(new SQLData("INSERT IGNORE INTO contacts (username,contact) values ('"+ txtSuche.getText()+ "','"+username+"')",'i',username));
-				
-			   txtSuche.setText("");
-			}
-			 // Client.sendSQL(new SQLData("INSERT INTO contacts (username,contact) values ('"+ username+ "','"+txtSuche.getText()+"') Where NOT EXISTS(SELECT * from contacts where username like '"+ username+ "' and contact like '"+txtSuche.getText()+"')",'i',username));
-			   // Client.sendSQL(new SQLData("INSERT INTO contacts (username,contact) values ('"+ txtSuche.getText()+ "','"+username+"') Where NOT EXISTS(SELECT * from contacts where username like '"+ txtSuche.getText()+ "' and contact like '"+username+"')",'i',username));
-				
-			    
-			 //   Client.sendSQL(new SQLData("IF NOT EXISTS (SELECT * from contacts where username like '"+ username+ "' and contact like '"+txtSuche.getText()+"') THEN INSERT INTO contacts (username,contact) values ('"+ username+ "','"+txtSuche.getText()+"') END IF",'i',username));
-			  //  Client.sendSQL(new SQLData("IF NOT EXISTS (SELECT * from contacts where username like '"+ txtSuche.getText()+ "' and contact like '"+username+"') THEN INSERT INTO contacts (username,contact) values ('"+ txtSuche.getText()+ "','"+username+"') END IF",'i',username));
-				   
-	
-			    
-			    //IF NOT EXISTS (SELECT xyz FROM TABLE WHERE col = 'xyz') THEN
-		                //INSERT INTO....
-		               // ELSE
-		               //                 UPDATE....
-		               // END IF;
-			    
-			    
-			   
-			   
-			  //  MultiThreadedServer.sqlBefehlsListe.add(new SQLData("UPDATE user set status='Online' where username like '"+this.user+"'",'n'));
-				
-		    
-		    }
-		    
-		});
-		btnKontaktSuche.setFont(new Font("Tahoma", Font.BOLD, 12));
-		panelKontakt.add(btnKontaktSuche);
-		
-		
-		if(username.equalsIgnoreCase("Admin")){
-		    btnKontaktSuche.setEnabled(false);
-		    txtSuche.setText("Admin hat alle");
-		    txtSuche.setEnabled(false);
-		    txtSuche.setEditable(false);
-		}
-		
-
-		frame.getContentPane().add(panelKontakt);
-		frame.add(new GradientPanel(new Color(27, 130, 165), new Color(204, 204,
-				255), width, height));
+		frame.getContentPane().add(mainPanel);
 	}
 
 	public static void PictureUpdater(String picID) {
-	    
-	
-	    String shortendPicID=picID.substring(0,1);
-	    System.out.println("picID:" + picID +"|"+shortendPicID);
-	    ImageIcon avatarImage = new ImageIcon();
-	    switch(shortendPicID){ 
-	        case "1": 
-	            System.out.println("pic1"); 
-	            avatarImage =i1;
-	            break; 
-	        case "2": 
-	            System.out.println("pic2"); 
-	            avatarImage =i2;
-	            break; 
-	        case "3": 
-	            System.out.println("pic3"); 
-	            avatarImage =i3;
-	            break; 
-	        case "4": 
-	            System.out.println("pic4"); 
-	            avatarImage =i4;
-	            break; 
-	        case "5": 
-	            System.out.println("pic5"); 
-	            avatarImage =i5;
-	            break; 
-	        case "6": 
-	            System.out.println("pic6"); 
-	            avatarImage =i6;
-	            break; 
-	        case "7": 
-	            System.out.println("pic7"); 
-	            avatarImage =i7;
-	            break; 
-	        case "8": 
-	            System.out.println("pic8"); 
-	            avatarImage =i8;
-	            break; 
-	        case "9": 
-	            System.out.println("pic9"); 
-	            avatarImage =i9;
-	            break; 
-	        default: 
-	            System.out.println("pic1"); 
-	            avatarImage =i1;
-	        } 
-	    label_2.setIcon(avatarImage);
-	    
+
+		String shortendPicID = picID.substring(0, 1);
+		System.out.println("picID:" + picID + "|" + shortendPicID);
+		ImageIcon avatarImage = new ImageIcon();
+		switch (shortendPicID) {
+		case "1":
+			System.out.println("pic1");
+			avatarImage = i1;
+			break;
+		case "2":
+			System.out.println("pic2");
+			avatarImage = i2;
+			break;
+		case "3":
+			System.out.println("pic3");
+			avatarImage = i3;
+			break;
+		case "4":
+			System.out.println("pic4");
+			avatarImage = i4;
+			break;
+		case "5":
+			System.out.println("pic5");
+			avatarImage = i5;
+			break;
+		case "6":
+			System.out.println("pic6");
+			avatarImage = i6;
+			break;
+		case "7":
+			System.out.println("pic7");
+			avatarImage = i7;
+			break;
+		case "8":
+			System.out.println("pic8");
+			avatarImage = i8;
+			break;
+		case "9":
+			System.out.println("pic9");
+			avatarImage = i9;
+			break;
+		default:
+			System.out.println("pic1");
+			avatarImage = i1;
+		}
+		label_2.setIcon(avatarImage);
+
 	}
 }
